cbd5e7836f51ec69a0222d6afa949e28e6fc8a85
Binkupi <77879610+Binkupi@users.noreply.github.com>
2021-07-31 22:45:42 +0700

    up

--- pages/icon.js/index.js
+++ pages/icon.js/index.js
@@ -1,51 +1,86 @@
-import Layout from '../../components/Layout'
-import {getIcons} from '../../lib/icon'
+// import Layout from '../../components/Layout'
+// import {getIcons} from '../../lib/icon'
+// import Card from 'react-bootstrap/Card';
+// import { useEffect } from 'react';
+// import Link from 'next/link'
+// export default function Icon({ 
+//     icons
+// })
+// {   
+//     const convertString=(text)=>{
+//         var lines =text.split('\n');
+//       for(var line = 0; line < lines.length; line++){    
+//             task(lines[line],line)    
+//       }
+      
+//     }
+//     function task(text,i) {
+//         setTimeout(function() {
+//           document.getElementById('content').innerHTML+=text;
+//           document.getElementById('content').innerHTML+='</br>';
+//           var scrollingElement = (document.scrollingElement || document.body);
+//             scrollingElement.scrollTop = scrollingElement.scrollHeight;
+//         }, 100 * i);
+//       }
+
+//     const showMe=(icons)=>{
+//         document.getElementById('btn-show').style.display='none';
+//         document.getElementById('cmt').style.display='block';
+//         convertString(icons[0].iconContent)
+        
+//     }
+//     return (
+//         <Layout>
+//             <Card className="my-3 shadow">
+//                 <Card.Body>
+//                     <Card.Title>
+//                         <div className="text-center">
+//                         <span >Bin Kupi</span> 
+//                         </div> 
+//                     </Card.Title>
+//                     <div className="text-center">
+//                         <button onClick={()=>showMe(icons)} id='btn-show' className='btn btn-primary text-align'>Xem hình ảnh của tôi</button>
+//                     </div>
+                    
+//                     <p id='content' style={{fontFamily:"monospace",
+//                                     fontSize:'1.2rem'}} className='text-center'>
+//                     </p>
+//                     <p className='text-center' id='cmt' style={{display:'none'}}>Đẹp không =))Thu nhỏ để dễ nhìn hơn</p>
+//                 </Card.Body>
+//                 {/* <Link href={`/posts/${post.id}`} passHref><button className="btn btn-primary">See more</button></Link> */}
+//             </Card>
+//         </Layout>
+        
+//     )
+// }
+// export const getStaticProps=async()=>{
+//     const icons=await getIcons();
+
+//     return {
+//         props:{
+//             icons
+//         }
+//     }
+// }
+import Layout from './../../components/Layout'
+import {getRandomJoke} from '../../lib/jokes'
 import Card from 'react-bootstrap/Card';
 import { useEffect } from 'react';
 import Link from 'next/link'
-export default function Icon({ 
-    icons
-})
-{   
-    const convertString=(text)=>{
-        var lines =text.split('\n');
-      for(var line = 0; line < lines.length; line++){    
-            task(lines[line],line)    
-      }
-      
-    }
-    function task(text,i) {
-        setTimeout(function() {
-          document.getElementById('content').innerHTML+=text;
-          document.getElementById('content').innerHTML+='</br>';
-          var scrollingElement = (document.scrollingElement || document.body);
-            scrollingElement.scrollTop = scrollingElement.scrollHeight;
-        }, 100 * i);
-      }
+export default function Jokes({ 
+    joke:joke
+}){
 
-    const showMe=(icons)=>{
-        document.getElementById('btn-show').style.display='none';
-        document.getElementById('cmt').style.display='block';
-        convertString(icons[0].iconContent)
-        
-    }
     return (
         <Layout>
             <Card className="my-3 shadow">
                 <Card.Body>
                     <Card.Title>
-                        <div className="text-center">
-                        <span >Bin Kupi</span> 
-                        </div> 
+                        <span>Here is a new story</span> 
                     </Card.Title>
-                    <div className="text-center">
-                        <button onClick={()=>showMe(icons)} id='btn-show' className='btn btn-primary text-align'>Xem hình ảnh của tôi</button>
-                    </div>
-                    
-                    <p id='content' style={{fontFamily:"monospace",
-                                    fontSize:'1.2rem'}} className='text-center'>
-                    </p>
-                    <p className='text-center' id='cmt' style={{display:'none'}}>Đẹp không =))Thu nhỏ để dễ nhìn hơn</p>
+                    <Card.Text>
+                       <span>{joke.value}</span>
+                    </Card.Text>
                 </Card.Body>
                 {/* <Link href={`/posts/${post.id}`} passHref><button className="btn btn-primary">See more</button></Link> */}
             </Card>
@@ -53,12 +88,11 @@ export default function Icon({
         
     )
 }
-export const getStaticProps=async()=>{
-    const icons=await getIcons();
-
+export const getServerSideProps=async()=>{
+    const joke=await getRandomJoke();
     return {
         props:{
-            icons
+            joke
         }
     }
 }
