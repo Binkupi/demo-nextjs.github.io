d2369b6584ccc54fd68bc0ca4a51ed448ce1e7e9
Binkupi <77879610+Binkupi@users.noreply.github.com>
2021-08-01 17:14:28 +0700

    up

--- components/Layout.js
+++ components/Layout.js
@@ -7,29 +7,29 @@ const Layout=({children})=>(
             <meta charSet="utf-8"/>
             <meta httpEquiv="X-UA-comatible" content ="IE-edge"/>
             <meta name="viewport" content="width=device-width, initial-scale=1"/>
-            <title>My Next app</title>
+            <title>Nhi Xinh đẹp</title>
         </Head>
         <header>
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container-fluid">
-                <Link href="/"><a className="navbar-brand" >My next app</a></Link>
+                <Link href="/"><a className="navbar-brand" >Nhi xinh đẹp</a></Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                     <li className="nav-item">
-                    <Link  href="/posts" passHref><a className="nav-link active" aria-current="page" >Post</a></Link>
+                    <Link  href="/jokes" passHref><a className="nav-link active" aria-current="page" >Post</a></Link>
                     </li>
                     <li className="nav-item">
                     <Link  href="/jokes" passHref><a className="nav-link">Images</a></Link>
                     </li>
                     <li className="nav-item">
-                        <Link  href="/about"><a className="nav-link">About</a></Link>
+                        <Link  href="/jokes"><a className="nav-link">About</a></Link>
                     
                     </li>
                     <li className="nav-item">
-                        <Link  href="/icon"><a className="nav-link">icon</a></Link>
+                        <Link  href="/jokes"><a className="nav-link">icon</a></Link>
                     
                     </li>
                     {/* <li className="nav-item dropdown">
--- pages/jokes/index.js
+++ pages/jokes/index.js
@@ -26,8 +26,18 @@ export default function Icon({
 
     const showMe=(icons)=>{
         document.getElementById('btn-show').style.display='none';
-        document.getElementById('cmt').style.display='block';
-        convertString(icons[0].iconContent)
+        document.getElementById('animation').style.display='block';
+        document.getElementById('loading').style.display='block';
+        setTimeout(function(){
+            document.getElementById('animation').style.display='none';
+            document.getElementById('loading').style.display='none';
+            
+            convertString(icons[0].iconContent);
+            document.getElementById('cmt').style.display='block';
+            },5000)
+             
+      
+        
         
     }
     return (
@@ -36,17 +46,43 @@ export default function Icon({
                 <Card.Body>
                     <Card.Title>
                         <div className="text-center">
-                        <span >Nhiiiiiiiiiii</span> 
+                        <span > Đây là tôi &lt;3 &lt;3</span> 
                         </div> 
                     </Card.Title>
                     <div className="text-center">
-                        <button onClick={()=>showMe(icons)} id='btn-show' className='btn btn-primary text-align'>Xem hình ảnh của tôi</button>
-                    </div>
-                    
+                        <button onClick={()=>showMe(icons)} id='btn-show' className='btn btn-primary text-align'>Đợi chờ chi nữa. Nhấp vào nào!!!</button>
+                    </div >
+                    <div id='animation' style={{display:'none'}} className="text-center mt-4">
+                        <div className="spinner-border text-primary" role="status">
+                            <span className="visually-hidden">Loading...</span>
+                            </div>
+                            <div className="spinner-border text-secondary" role="status">
+                            <span className="visually-hidden">Loading...</span>
+                            </div>
+                            <div className="spinner-border text-success" role="status">
+                            <span className="visually-hidden">Loading...</span>
+                            </div>
+                            <div className="spinner-border text-danger" role="status">
+                            <span className="visually-hidden">Loading...</span>
+                            </div>
+                            <div className="spinner-border text-warning" role="status">
+                            <span className="visually-hidden">Loading...</span>
+                            </div>
+                            <div className="spinner-border text-info" role="status">
+                            <span className="visually-hidden">Loading...</span>
+                            </div>
+                            <div className="spinner-border text-light" role="status">
+                            <span className="visually-hidden">Loading...</span>
+                            </div>
+                            <div className="spinner-border text-dark" role="status">
+                            <span className="visually-hidden">Loading...</span>
+                            </div>
+                        </div>
+                    <p className="mt-3 mb-3 text-center" id="loading" style={{display:'none'}}>Đợi chờ Nhiii nào &lt;3</p>
                     <p id='content' style={{fontFamily:"monospace",
-                                  fontSize:'1px',fontHeight:'1px' }} className='text-center'>
+                                  fontSize:'5px',fontHeight:'5px' }} className='text-center'>
                     </p>
-                    <p className='text-center' id='cmt' style={{display:'none'}}>Đẹp không =))zoom nhỏ chrome để dễ nhìn hơn nè</p>
+                    <p className='text-center' id='cmt' style={{display:'none'}}>Đẹp không =)) Zoom nhỏ trình duyệt để nhìn hơn nè</p>
                 </Card.Body>
                 {/* <Link href={`/posts/${post.id}`} passHref><button className="btn btn-primary">See more</button></Link> */}
             </Card>
